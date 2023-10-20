33a777b40a67241df4e1dd5ff4c1f70fdbe734fb
Thomas Stringer <thomas@trstringer.com>
2023-10-19 23:49:15 -0400

    Fix find

--- setup.sh
+++ setup.sh
@@ -5,4 +5,4 @@ echo "Running initial setup..."
 NEW_BASENAME=$(basename $(pwd))
 
 # Replace the old project name with the new one in all files.
-find -type f . -name "*" | xargs -rn 1 sed -i -e "s/go-template-cli-simple/$NEW_BASENAME/g"
+find . -type f | xargs -rn 1 sed -i -e "s/go-template-cli-simple/$NEW_BASENAME/g"
