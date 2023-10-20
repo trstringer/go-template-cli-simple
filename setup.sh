082f908083d677eca5fd4feaed0ed8ca738cac71
Thomas Stringer <thomas@trstringer.com>
2023-10-19 23:54:28 -0400

    Add readme reset to setup

--- setup.sh
+++ setup.sh
@@ -6,3 +6,6 @@ NEW_BASENAME=$(basename $(pwd))
 
 # Replace the old project name with the new one in all files.
 find . -type f | xargs -rn 1 sed -i -e "s/go-template-cli-simple/$NEW_BASENAME/g"
+
+# Reset the README, as it currently contains template instructions.
+echo "# $NEW_BASENAME" > README.md
